bf51fb558a19d8af774946a76a46ac78760d3532
Paula Moreta <moretaap@campusemav.com>
2020-10-19 09:58:58 +0200

    Classe 19/10/2020
    Fins puntuació pickups

--- Assets/Scripts/ScrPlayer.cs
+++ Assets/Scripts/ScrPlayer.cs
@@ -5,21 +5,41 @@ using UnityEngine;
 public class ScrPlayer : MonoBehaviour
 {
     [SerializeField]
-    float velocitat = 3;
+    float forsa = 3;
+    float x, y; //variables per guardar lectura dels cursors de forma global
+
+    //Vull definir un objecte del tipus Rigidbody anomenat rb (variable), val 0. Per accedir al component RigidBody:
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); //ara rb apunta al component rigidBody del player AIXÒ ÉS BÀSIC
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x, y; //per llegir gamepad
         x = Input.GetAxis("Horizontal"); //llegir moviment horizontal
         y = Input.GetAxis("Vertical"); //llegir moviment vertical
-        //print("Horizontal: " + x + "/ Vertical: " + y); //mostrem x i y
-
-        transform.Translate(x * Time.deltaTime * velocitat, y * Time.deltaTime * velocitat, 0);
+        
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(x*forsa, y*forsa));
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (collision.tag == "Pickup") Destroy(collision.gameObject);
+        if (collision.CompareTag("Pickup"))
+        {
+            Destroy(collision.gameObject);
+            ScrControlGame.punts += 5;
+            print("Puntuació " + ScrControlGame.punts);
+        }
+            
+            
+            
+            
     }
 }
