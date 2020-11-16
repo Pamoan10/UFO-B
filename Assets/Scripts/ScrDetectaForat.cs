a239205de4c3318c7c89a8d6c68b1a11816d5882
Paula Moreta <moretaap@campusemav.com>
2020-11-16 10:30:27 +0100

    Classe 16/11/2020
    Hem afegit forat negre i la porta

---
+++ Assets/Scripts/ScrDetectaForat.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrDetectaForat : MonoBehaviour
+{
+    // Start is called before the first frame update
+  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        float escala = 0; 
+        if (collision.CompareTag("Forat") && GetComponentInParent<Rigidbody2D>().velocity.magnitude < 3)
+        {
+            GetComponentInParent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            transform.parent.position = Vector3.Lerp(transform.parent.position, collision.transform.position, .05f);
+
+            escala = transform.parent.localScale.x;
+            if (escala > 0)
+            {
+                escala -= .03f;
+                transform.parent.localScale = new Vector3(escala, escala, 1);
+            }
+            else print("ja ha acabat la caiguda, fer el que calgui ara...");
+        }
+
+    }
+}
---
+++ Assets/Scripts/ScrPorta.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrPorta : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] SpriteRenderer porta; //Assigno el sprite al que vull accedir
+
+    bool obrint = false, tancant = false; //indico si la porta s'obre o es tanca
+    float tamObert, tamTancat; //Mida de la porta oberta i tancada
+    void Start()
+    {
+        tamObert = porta.size.y; //el que mesura la porta oberta
+        tamTancat = 0.4f; // el que vull que mesuri quan la tanqui
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O) && !tancant) obrint = true;
+        if (Input.GetKeyDown(KeyCode.T) && !obrint) tancant = true;
+        if (obrint) Obrir();
+        if (tancant) Tancar();
+    }
+    void Obrir()
+    {
+        porta.size = Vector2.Lerp(porta.size, new Vector2(0.66f, tamTancat), .1f);
+        if (porta.size.y <= tamTancat + 0.01f)
+        {
+            obrint = false;
+        }
+    }
+    void Tancar()
+    {
+        porta.size = Vector2.Lerp(porta.size, new Vector2(0.66f, tamObert), .1f);
+        if(porta.size.y >= tamObert - 0.01f)
+        {
+            tancant = false;
+        }
+    }
+}
